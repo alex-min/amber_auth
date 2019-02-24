bbb42873d837447a10a529592b2507398454a30e
Alexandre MINETTE <alex@hunted.com>
2019-02-24 11:51:16 +0700

    copying the templates from amber

--- src/amber_auth.cr
+++ src/amber_auth.cr
@@ -1,4 +1,5 @@
 # TODO: Write documentation for `AmberAuth`
+require "./rendering_entry.cr";
 module AmberAuth
   VERSION = "0.1.0"
 
---
+++ src/cli.cr
@@ -0,0 +1,27 @@
+require "cli"
+
+module AmberAuth::CLI
+  class MainCommand < ::Cli::Supercommand
+    command_name "amber_auth"
+
+    class Help
+      title "\nAmber Auth CLI"
+      header <<-EOS
+        The amber_auth command generate authentication files for a user, it generates:
+         - Signup view
+         - Signin view
+         - Forgotten password view & mailer
+         - migrations
+
+        Execute this command in your current directory
+
+      EOS
+
+      footer <<-EOS
+      Example:
+      crystal lib/amber_auth/src/cli.cr generate
+      EOS
+    end    
+  end
+end
+AmberAuth::CLI::MainCommand.run ARGV
